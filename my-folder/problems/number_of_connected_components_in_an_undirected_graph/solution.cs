a607e3e18f93456701a6eadbab56717089a4932e
Mohit Kumar Verma <mkvmb.6@gmail.com>
2022-11-27 16:39:06 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (csharp)

---
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.cs
@@ -0,0 +1,57 @@
+public class Solution {
+    public int CountComponents(int n, int[][] edges) {
+        var unionFind = new UnionFind(n);
+        foreach(var edge in edges){
+            unionFind.Union(edge[0], edge[1]);
+        }
+        return unionFind.Count;
+    }
+}
+
+
+
+public class UnionFind{
+    private int[] roots;
+    private int[] ranks;
+    
+    public int Count{get;private set;}
+    
+    public UnionFind(int size){
+        roots = new int[size];
+        ranks = new int[size];
+        Count = size;
+        for(int i=0;i<size;i++){
+            roots[i]=i;
+            ranks[i]=1;
+        }
+    }
+    
+    public int Find(int x){
+        var root = roots[x];
+        if(x == root){
+            return x;
+        }
+        roots[x]=Find(root);
+        return roots[x];
+    }
+    
+    public bool Union(int x, int y){
+        var rootX = Find(x);
+        var rootY = Find(y);
+        if(rootX != rootY){
+            if(ranks[rootX] > ranks[rootY]){
+                roots[rootY] = rootX;
+            }
+            else if(ranks[rootY] > ranks[rootX]){
+                roots[rootX] = rootY;
+            }
+            else{
+                roots[rootY] = rootX;
+                ranks[rootX]++;
+            }
+            Count--;
+            return true;
+        }
+        return false;
+    }
+}
