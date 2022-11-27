c0f7becedb2c3b301abeb06cb6b7a4a57e7c5bca
Mohit Kumar Verma <mkvmb.6@gmail.com>
2022-11-27 06:27:32 +0000

    Sync LeetCode submission - Longest Palindromic Substring (csharp)

---
+++ my-folder/problems/longest_palindromic_substring/solution.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if(string.IsNullOrEmpty(s)){
+            return string.Empty;
+        }
+        int start = 0, end = 0;
+        for(int i=0;i<s.Length;i++){
+            var len1 = GetLengthFromCenter(s, i, i);
+            var len2 = GetLengthFromCenter(s, i, i+1);
+            var len = Math.Max(len1, len2);
+            if(len > end - start){
+                start = i - (len-1)/2;
+                end = i + len/2;
+            }
+        }
+        return s.Substring(start, end-start+1);
+    }
+    
+    int GetLengthFromCenter(string s, int left, int right){
+        var len = s.Length;
+        while(left >= 0 && right < len && s[left]==s[right]){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
