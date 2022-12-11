6fdead02fe954739ddef9b54be62e8d4d099d03e
Mohit Kumar Verma <mkvmb.6@gmail.com>
2022-12-11 16:32:31 +0000

    Sync LeetCode submission - Subsets II (csharp)

--- my-folder/problems/subsets_ii/solution.cs
+++ my-folder/problems/subsets_ii/solution.cs
@@ -1,21 +1,22 @@
 public class Solution {
     public IList<IList<int>> SubsetsWithDup(int[] nums) {
-        Array.Sort(nums);
-        var result = new List<IList<int>>{new List<int>()};
-        var prevNum = int.MinValue;
-        var prevCount = 0;
-        foreach(var num in nums){
-            var count = result.Count;
-            var start = prevNum == num ? prevCount : 0;
-            for(int i=start;i<count;i++){
-                var newList = result[i].ToList();
-                newList.Add(num);
-                result.Add(newList);
-            }
-            
-            prevCount = count;
-            prevNum = num;
-        }
+        var result = new List<IList<int>>();
+        Array.Sort(nums);    
+        FindSubsets(0, nums, new List<int>(), result);
         return result;
+    }
+
+    void FindSubsets(int index, int[] nums, IList<int> pickedItems, IList<IList<int>> result){
+        if(index==nums.Length){
+            result.Add(pickedItems.ToList());
+            return;
+        }
+        pickedItems.Add(nums[index]);
+        FindSubsets(index+1, nums, pickedItems, result);
+        pickedItems.Remove(nums[index]);
+        while(index<nums.Length-1 && nums[index]==nums[index+1]){
+            index++;
+        }
+        FindSubsets(index+1, nums, pickedItems, result);
     }
 }
