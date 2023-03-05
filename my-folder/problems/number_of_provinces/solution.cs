f50a50ddca50f3995e73eb004328d1781d950865
Mohit Kumar Verma <mkvmb.6@gmail.com>
2023-03-05 09:35:58 +0000

    Sync LeetCode submission - Number of Provinces (csharp)

--- my-folder/problems/number_of_provinces/solution.cs
+++ my-folder/problems/number_of_provinces/solution.cs
@@ -1,59 +1,27 @@
 public class Solution {
     public int FindCircleNum(int[][] isConnected) {
         var len = isConnected.Length;
-        var unionFind = new UnionFind(len);
+        var visited = new int[len];
+        var provinces = 0;
         for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                if(isConnected[i][j]==1){
-                    unionFind.Union(i, j);
-                }                
+            if(visited[i]==0){
+                provinces++;
+                VisitConnectedCities(i, isConnected, visited, len);
             }
         }
-        
-        return unionFind.Count;
-    }
-}
 
-public class UnionFind{
-    private int[] roots;
-    private int[] ranks;
-    
-    public int Count{get;private set;}
-    
-    public UnionFind(int size){
-        roots = new int[size];
-        ranks = new int[size];
-        Count = size;
-        for(int i=0;i<size;i++){
-            roots[i]=i;
-            ranks[i]=1;
-        }
+        return provinces;
     }
-    
-    public int Find(int x){
-        var root = roots[x];
-        if(x == root){
-            return x;
+
+    void VisitConnectedCities(int i, int[][] isConnected, int[] visited, int len){
+        if(visited[i]==1){
+            return;
         }
-        roots[x]=Find(root);
-        return roots[x];
-    }
-    
-    public void Union(int x, int y){
-        var rootX = Find(x);
-        var rootY = Find(y);
-        if(rootX != rootY){
-            if(ranks[rootX] > ranks[rootY]){
-                roots[rootY] = rootX;
+        visited[i]=1;
+        for(int j=0;j<len;j++){
+            if(isConnected[i][j]==1){
+                VisitConnectedCities(j, isConnected, visited, len);
             }
-            else if(ranks[rootY] > ranks[rootX]){
-                roots[rootX] = rootY;
-            }
-            else{
-                roots[rootY] = rootX;
-                ranks[rootX]++;
-            }
-            Count--;
         }
     }
 }
