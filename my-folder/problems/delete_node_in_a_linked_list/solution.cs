cb321c6380a063b8df88e2de49f952d2a732ba1d
Mohit Kumar Verma <mkvmb.6@gmail.com>
2023-03-12 12:06:39 +0000

    Sync LeetCode submission - Delete Node in a Linked List (csharp)

---
+++ my-folder/problems/delete_node_in_a_linked_list/solution.cs
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void DeleteNode(ListNode node) {
+        var next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+        next.next = null;
+    }
+}
