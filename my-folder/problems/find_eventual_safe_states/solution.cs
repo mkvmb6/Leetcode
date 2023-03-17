104e2a1b38da67db33e7e5c52faa640f8b08cb47
Mohit Kumar Verma <mkvmb.6@gmail.com>
2023-03-17 14:17:11 +0000

    Sync LeetCode submission - Find Eventual Safe States (csharp)

--- my-folder/problems/find_eventual_safe_states/solution.cs
+++ my-folder/problems/find_eventual_safe_states/solution.cs
@@ -1,12 +1,36 @@
 public class Solution {
     public IList<int> EventualSafeNodes(int[][] graph) {
         var V = graph.Length;
-        var visited = new bool[V];
-        var pathVisited = new bool[V];
         var safeNodes = new List<int>();
         var safeMarked = new bool[V];
+        var inDegree = new int[V];
+        var adj = new Dictionary<int, List<int>>();
         for(int i=0;i<V;i++){
-            isCyclic(i, graph, visited, pathVisited, safeMarked);
+            adj[i]=new List<int>();
+        }
+        for(int i=0;i<V;i++){
+            foreach(var item in graph[i]){
+                inDegree[i]++;
+                adj[item].Add(i);
+            }
+        }
+
+        var queue = new Queue<int>();
+        for(int i=0;i<V;i++){
+            if(inDegree[i]==0){
+                queue.Enqueue(i);
+            }
+        }
+
+        while(queue.Any()){
+            var node = queue.Dequeue();
+            foreach(var item in adj[node]){
+                inDegree[item]--;
+                if(inDegree[item]==0){
+                    queue.Enqueue(item);
+                }
+            }
+            safeMarked[node]=true;
         }
         for(int i=0;i<V;i++){
             if(safeMarked[i]){
@@ -14,25 +38,5 @@ public class Solution {
             }
         }
         return safeNodes;
-    }
-    
-    bool isCyclic(int node, int[][] adj, bool[] visited, bool[] pathVisited, bool[] safeMarked){
-        if(visited[node]){
-            return false;
-        }
-        
-        visited[node] = true;
-        pathVisited[node] = true;
-        foreach(var child in adj[node]){
-            if(pathVisited[child]){
-                return true;
-            }
-            if(isCyclic(child, adj, visited, pathVisited, safeMarked)){
-                return true;
-            }
-        }
-        pathVisited[node] = false;
-        safeMarked[node] = true;
-        return false;
     }
 }
