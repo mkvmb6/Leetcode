8541a45ecafb128d615b9e12be27548bcf1cc561
Mohit Kumar Verma <mkvmb.6@gmail.com>
2023-06-28 03:21:05 +0000

    Sync LeetCode submission - Set Matrix Zeroes (csharp)

--- my-folder/problems/set_matrix_zeroes/solution.cs
+++ my-folder/problems/set_matrix_zeroes/solution.cs
@@ -2,24 +2,38 @@ public class Solution {
     public void SetZeroes(int[][] matrix) {
         var n = matrix.Length;
         var m = matrix[0].Length;
-        var row = new bool[n];
-        var col = new bool[m];
+        var colNeedsToBeZero = false;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            if(matrix[i][0]==0){
+                colNeedsToBeZero = true;
+            }
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i] = true;
-                    col[j] = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i] || col[j]){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j]=0;
                 }
+            }
+        }
+
+        if(matrix[0][0]==0){
+            for(int i=1;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(colNeedsToBeZero){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
 
