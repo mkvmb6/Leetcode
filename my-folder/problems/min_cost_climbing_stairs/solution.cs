23b669a3cd4b7e4f667321fd5bbfcc9336f2b56f
Mohit Kumar Verma <mkvmb.6@gmail.com>
2023-08-04 18:35:53 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (csharp)

--- my-folder/problems/min_cost_climbing_stairs/solution.cs
+++ my-folder/problems/min_cost_climbing_stairs/solution.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
-        var cache = new Dictionary<int, int>();
+        var cache = new int[cost.Length];
         return Math.Min(MinCostClimbingStairs(0, cost, cache), MinCostClimbingStairs(1, cost, cache));
     }
 
-    int MinCostClimbingStairs(int index, int[] cost, Dictionary<int, int> cache) {
+    int MinCostClimbingStairs(int index, int[] cost, int[] cache) {
         if(index>=cost.Length){
             return 0;
         }
@@ -12,7 +12,7 @@ public class Solution {
             return cost[index];
         }
         
-        if(cache.ContainsKey(index)){
+        if(cache[index] != 0){
             return cache[index];
         }
         var res = Math.Min(cost[index]+MinCostClimbingStairs(index+1, cost, cache), cost[index]+MinCostClimbingStairs(index+2, cost, cache));
